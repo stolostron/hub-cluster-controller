51a144fcc06a462f046fafcd1a4b67876cd98073
Chunlin Yang <chuyang@redhat.com>
2022-02-23 08:52:50 +0800

    Support custom MCH from annontation of managedcluster (#1)
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- pkg/cluster/manifestwork_test.go
+++ pkg/cluster/manifestwork_test.go
@@ -1,12 +1,31 @@
 package cluster
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 )
 
-func TestCreateSubManifestwork(t *testing.T) {
-	sub := CreateSubManifestwork("test")
-	if sub.GetName() != "test-"+HOH_HUB_CLUSTER_SUBSCRIPTION {
-		t.Fatalf("error")
+func TestCreateMCHManifestwork(t *testing.T) {
+	mch, _ := CreateMCHManifestwork("test", `{
+		"apiVersion": "operator.open-cluster-management.io/v1",
+		"kind": "MultiClusterHub",
+		"metadata": {
+			"name": "multiclusterhub",
+			"namespace":"open-cluster-management"
+		},
+		"spec": {
+			"imagePullSecret": "multiclusterhub-operator-pull-secret"
+		}
+	}`)
+	if mch.GetName() != "test-"+HOH_HUB_CLUSTER_MCH {
+		t.Fatalf("failed to find the %s manifestwork", "test-"+HOH_HUB_CLUSTER_MCH)
+	}
+	mchByte, err := json.Marshal(mch)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if !strings.Contains(string(mchByte), "disableHubSelfManagement") {
+		t.Fatalf("failed to find disableHubSelfManagement")
 	}
 }
